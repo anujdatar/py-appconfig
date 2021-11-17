7208aa6c61a8b1ee83abcb7989f6df01a0cffc87
Anuj Datar <anuj.datar@gmail.com>
2021-11-17 20:58:11 +0530

    Updated tests to check the new defaults, reset, reset all, delete funcs

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,20 @@ from os.path import exists
 import shutil
 from appconfig import AppConfig
 
-a = AppConfig(project_name='test', verbose=True)
+_defaults = {
+    'a': 10,
+    'b': 'this is a b'
+}
+
+_conf = {
+    'projectName': 'test',
+    'projectId': 'test',
+    'version': '0.0.1',
+    'a': 10,
+    'b': 'this is a b'
+}
+
+a = AppConfig(project_name='test', verbose=True, defaults=_defaults)
 
 
 def test_file_creation():
@@ -10,23 +23,52 @@ def test_file_creation():
     assert exists(filepath)
 
 
-# write test configuration values to
-test_config = {
-    "number": 1234,
-    "string": "some string value"
-}
-for (key, value) in test_config.items():
-    a.set(key, value)
-a.write_conf()
+def test_get():
+    assert a.get('projectName') == 'test'
+    assert a.get('projectId') == 'test'
+    assert a.get('version') == '0.0.1'
 
 
 def test_get_all_function():
-    assert a.get_all() == test_config
+    assert a.get_all() == _conf
 
 
-def test_individual_config_vales():
-    assert a.get("number") == 1234
-    assert a.get("string") == "some string value"
+def test_defaults():
+    assert a.get('a') == 10
+    assert a.get('b') == 'this is a b'
+
+
+def test_set():
+    a.set('a', 1000)
+    assert a.get('a') == 1000
+
+
+def test_reset():
+    a.reset('a')
+    assert a.get('a') == 10
+
+
+def test_set_new():
+    _test_config = {
+        'number': 1234,
+        'string': 'some string value'
+    }
+    for (key, value) in _test_config.items():
+        a.set(key, value)
+
+    assert a.get('number') == 1234
+    assert a.get('string') == 'some string value'
+
+
+def test_reset_all():
+    a.reset_all()
+    assert a.get_all() == _conf
+
+
+def test_delete():
+    del _conf['b']
+    a.delete('b')
+    assert a.get_all() == _conf
 
 
 def test_do_cleanup():
